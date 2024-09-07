c7ebe928a04c00b535af6c710f7b6ad5a67d0cba
Abhiayush-bit <132006132+Abhiayush-bit@users.noreply.github.com>
2024-09-07 14:29:17 +0000

    first_commit

---
+++ testfile.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    printf("hello world");
+}
+
+/* Compiler looks into 3 places to find the io header file
+1. current directory
+2. to a particualr directory by giving command -I
+3. to a known preconfigured standard directory. 
+
+int a; = This is a declaration and definition. Memory is being defined for this variable
+extern int a = This is only a declaration not defining it.
+for loop - should be used with array
+while loop - should be used with pointers
+printf , scanf - lib c file*/
